32286b538c0940650aba737e83b978a858ad903c
erxclau <ericlau41802@gmail.com>
2022-11-19 23:16:35 -0500

    Fix path resolution and nunjucks import

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 import { existsSync, readFileSync } from "node:fs";
-import { basename, extname, normalize } from "node:path";
-import { configure, renderString } from "nunjucks";
+import { basename, extname, join } from "node:path";
+import pkg from "nunjucks";
+const { configure, renderString } = pkg;
 
 export default () => ({
   name: "vite-plugin-transform-nunjucks",
   transformIndexHtml: {
     enforce: "pre",
     async transform(html) {
-      const configPath = new URL("config.json", import.meta.url);
+      const configPath = join(process.cwd(), "config.json");
       const config = JSON.parse(readFileSync(configPath));
+
       const json = Object.fromEntries(
         await Promise.all(
           config.fetch.filter(
             file => extname(file.output) === ".json"
               && file.id.length > 0
-              && file.output.lengeth > 0
-              && existsSync(new URL(file.output, import.meta.url))
+              && file.output.length > 0
+              && existsSync(join(process.cwd(), file.output))
           ).map(async (file) =>
           ([
             basename(file.output, ".json"),
             JSON.parse(
               readFileSync(
-                new URL(normalize(file.output, import.meta.url))
+                join(process.cwd(), file.output)
               )
             )
           ]))
