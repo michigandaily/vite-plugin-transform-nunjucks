c91160dd9ed84a981284973ea44f6885ff2db555
Eric Lau <44459660+erxclau@users.noreply.github.com>
2024-02-20 21:45:35 -0500

    Fix deprecations and missing config (#5)
    * Use new API
    
    * Remove config

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ import nunjucks from "nunjucks";
 export default (options = {}) => ({
   name: "vite-plugin-transform-nunjucks",
   transformIndexHtml: {
-    enforce: "pre",
-    async transform(html, { filename }) {
+    order: "pre",
+    async handler(html, { filename }) {
       nunjucks.configure({ autoescape: false });
       return nunjucks.renderString(html, {
-        config,
         filename: basename(filename),
         env: process.env.NODE_ENV,
         ...options,
